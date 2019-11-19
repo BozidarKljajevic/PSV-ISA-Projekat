e85f765d0594f25675260fcfa8c77ab7c0e18fa8
DamjanPantic <panticdamjan@gmail.com>
2019-11-19 17:43:47 +0100

    pravljenje modela za pacijenta

---
+++ src/main/java/com/example/demo/controller/PacijentController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.PacijentDTO;
+import com.example.demo.model.Pacijent;
+import com.example.demo.service.PacijentService;
+
+@RestController
+@RequestMapping(value = "pacijent")
+@CrossOrigin(origins = "http://localhost:8080")
+public class PacijentController {
+
+	@Autowired
+	private PacijentService pacijentService;
+	
+	@GetMapping(value = "/postojeciPacijent")
+	public ResponseEntity<PacijentDTO> getPostojeciPacijent() {
+		
+		Pacijent pacijent = pacijentService.findOne((long) 1);
+		
+		PacijentDTO pacijentDTO = new PacijentDTO(pacijent);
+		
+		return new ResponseEntity<>(pacijentDTO, HttpStatus.OK);
+	}
+}
