ede4e1d901eebfb080e303828f4416d6aec5b3fb
BozidarKljajevic <bozidar.kljajevic07@gmail.com>
2019-11-20 20:30:32 +0100

    Dodat administrator klinickog centra

---
+++ src/main/java/com/example/demo/repository/AdminCentraRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.AdminCentra;
+
+public interface AdminCentraRepository extends JpaRepository<AdminCentra, Long> {
+
+}
